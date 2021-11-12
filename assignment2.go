bc252e86a57b54b0901357270c074a6cd777fd2b
Anderson Fraga <kaspa@onionmail.org>
2021-11-12 22:26:55 +0900

    Init assignmt 3

--- assignment2.go
+++ assignment2.go
@@ -1,7 +1,25 @@
+//- Use var para declarar três variáveis. Elas devem ter package-level scope. Não atribua valores a estas variáveis. Utilize os seguintes identificadores e tipos para estas variáveis:
+//		Identificador "x" deverá ter tipo int
+//		Identificador "y" deverá ter tipo string
+//		Identificador "z" deverá ter tipo bool
+//- Na função main:
+//		Demonstre os valores de cada identificador
+//		O compilador atribuiu valores para essas variáveis. Como esses valores se chamam?
+
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Printf("%v, %T\n", x, x)
+	fmt.Printf("%v, %T\n", y, y)
+	fmt.Printf("%v, %T\n", z, z)
 }
 
-// golang playground link here
+// https://play.golang.org/p/PmTebAQr0xu
---
+++ assignment3.go
@@ -0,0 +1,7 @@
+package main
+
+//	"fmt"
+
+//func main() {
+
+//}
