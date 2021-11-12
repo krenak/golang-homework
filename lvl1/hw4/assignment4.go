b99e43813fb747cc12ecd799eddce22cb5d94db7
Anderson Fraga <kaspa@onionmail.org>
2021-11-13 02:57:23 +0900

    Init assignmt 4

---
+++ lvl1/hw4/assignment4.go
@@ -0,0 +1,31 @@
+// - Create a type. The underlying type must be int.
+// - Create a variable for this type, with the identifier "x", using the var keyword.
+// - In the main function:
+//      1. Show the value of the variable "x"
+//      2. Demonstrate the type of variable "x"
+//      3. Assign 42 to variable "x" using the operator "="
+//      4. Show the value of the variable "x"
+// - For the adventurous: https://golang.org/ref/spec#Types
+// - Now we are almost ninjas lvl 1!
+// - Solution: https://play.golang.org/p/snm4WuuYmG
+
+package main
+
+import (
+	"fmt"
+)
+
+type (
+	ninja int
+)
+
+var x ninja
+
+func main() {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Println(x)
+}
+
+//
