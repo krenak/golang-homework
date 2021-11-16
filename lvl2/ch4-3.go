248e1198fa88909a9ac52039a9264746102533cc
Anderson Fraga <kaspa@onionmail.org>
2021-11-17 03:26:48 +0900

    Init level 2

--- lvl2/ch4-3.go
+++ lvl2/ch4-3.go
@@ -5,16 +5,25 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func main() {
-	a := "e"
-	b := "é"
-	c := "香"
+	a := "e" // byte allocation with 1 byte
+	b := "é" // byte allocation with 2 byte
+	c := "香" // byte allocation with 3 byte
 	fmt.Printf("%v, %v, %v\n", a, b, c)
 
-	d := []byte(a)
-	e := []byte(b)
-	f := []byte(c)
+	d := []byte(a) // slice showing "a" byte allocation
+	e := []byte(b) // slice showing "b" byte allocation
+	f := []byte(c) // slice showing "c" byte allocation
 	fmt.Printf("%v, %v, %v\n", d, e, f)
+
+	g := 10   // integer
+	h := 10.1 // float point
+	fmt.Printf("%v, %T\n", g, g)
+	fmt.Printf("%v, %T\n", h, h)
+
+	fmt.Println(runtime.GOOS)   // identify Operational System
+	fmt.Println(runtime.GOARCH) // identify architecture
 }
