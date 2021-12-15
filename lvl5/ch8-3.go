b85388fb4ab2e28284f9e1bc29f7a91bbecfd325
Anderson Fraga <kaspa@onionmail.org>
2021-12-15 10:44:09 +0900

    Add Ch8-Ep_3-4

--- lvl5/ch8-3.go
+++ lvl5/ch8-3.go
@@ -1,6 +1,6 @@
 // Data structure - Range (Chapter 8 - Episode 3)
 // https://go.dev/ref/spec#Composite_literals
-// https://gobyexample.com/ranges
+// https://gobyexample.com/range
 // https://youtu.be/l6O8HWaoy_w
 
 package main
@@ -11,7 +11,20 @@ import(
 
 func main() {
 	slice := []string{"banana", "apple", "watermelon"}
-	for índice, valor := range slice {
+//	for índice, valor := range slice {
+//		fmt.Println("No índice", índice,"temos o valor:", valor)
+//	}
+
+	slice = append(slice, "coxinha")
+	for índice, valor := range slice { // range iterates over elements in 
+					   // a variety of data structures
 		fmt.Println("No índice", índice,"temos o valor:", valor)
 	}
+	// another example provided by Ellen's
+	slice1 := []int{20,21,22,23,1,13}
+	total := 0
+	for _, valor1 := range slice1 { // the before índice value is discharged
+		total += valor1		// and here total is summed with valor1
+	}
+	fmt.Println("O valor total é:", total)
 }
---
+++ lvl5/ch8-4.go
@@ -0,0 +1,14 @@
+// Data structure - Slice of slices (Chapter 8 - Episode 4)
+// https://go.dev/ref/spec#Making_slices_maps_and_channels
+// https://gobyexample.com/slices
+// https://youtu.be/G0rxcnojV_U
+
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+
+}
