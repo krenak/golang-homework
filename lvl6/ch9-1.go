63095d706a50eecd61c5d04b4e800b1486ded3e5
Anderson Fraga <kaspa@onionmail.org>
2022-01-24 23:08:52 +0900

    Assignments lvl4

---
+++ lvl6/ch9-1.go
@@ -0,0 +1,24 @@
+/* Assignment 14 (Chapter 9 - episode 1)
+- Using a composite literal:
+      - Create an array that supports 5 values of type int
+      - Assign values to your indexes
+- Use range and show the values of the array.
+- Using format printing, demonstrate the array type.
+- Solution: https://play.golang.org/p/tpWDzzlO2l */
+
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	array := [5]int{1,3,5,7,9}
+	fmt.Println(array)
+	for _, v := range array {
+		fmt.Println(v)
+		fmt.Printf("%T", array)
+	}
+}
+
+
